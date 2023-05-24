40d24bf767054602a2210bef7eb642cc4e0ad859
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-05-24 10:07:30 -0300

    ejs views

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const path = require('path');
+
+// importing routes
+
 const checklistsRouter = require('./src/routes/checklists');
+const rootRouter = require('./src/routes/index');
+
+// db server
+
 require('./config/database');
 
 const app = express();
 const route = 3000;
 
-app.use('/checklists', checklistsRouter)
+// setting views directory and engine
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+// static files serving
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// defining routes
+
+app.use('/checklists', checklistsRouter);
+app.use('/', rootRouter);
+
+
+
+// server listening 
 
 app.listen(route, () => {console.log(`Server listening at: http://localhost:${route}`)});
--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Checklist = require('../model/checklist');
+const methodOverride = require('method-override');
 
 // creating router
 
@@ -7,17 +8,29 @@ const router = express.Router();
 
 // try to parse received json and put on requisition's body object
 
-router.use(express.json())
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+router.use(methodOverride('_method'));
 
 // GET all method
 
 router.get('/', async (req, res) => {
     try {
         let checklists = await Checklist.find();
-        res.status(200).json(checklists);
+        res.status(200).render('checklists/index', { checklists: checklists });
     }
     catch(err) {
-        res.status(500).json(err);
+        res.status(500).render('pages/error', { error: err });
+    }
+});
+
+router.get('/new', async (req, res) => {
+    try {
+        let checklist = new Checklist();
+        res.status(200).render('checklists/new', { checklist: checklist });
+    }
+    catch(err) {
+        res.status(500).render('pages/error', { error: err });
     }
 });
 
@@ -26,24 +39,25 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findById(req.params.id);
-        res.status(200).json(checklist);
+        res.status(200).render('checklists/show', { checklist: checklist });
     }
     catch(err) {
-        res.status(404).json(err);
+        res.status(404).render('pages/error', { error: err });
     }
 })
 
 // POST method
 
 router.post('/', async (req, res) => {
-    let name = req.body.name;
+    let { name } = req.body.checklist;
+    let checklist = new Checklist({name})
 
     try {
-        let checklist = await Checklist.create({ name });
-        res.status(200).json(checklist);
+        await checklist.save();
+        res.redirect('/checklists');
     }
     catch(err) {
-        res.status(422).json(err);
+        res.status(422).render('checklists/new', {checklists: { ...checklist, err }});
     }
 });
 
